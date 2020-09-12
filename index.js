71b8838f76c63187ed5d079770c86ba457ce4c77
Chris Bell <cbell@acculynx.com>
2020-09-12 18:04:21 -0500

    get rid of bearer auth

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 const http = require('http');
 const Twitter = require('twitter');
 
-// const client = new Twitter({
-//     consumer_key: process.env.TWITTER_CONSUMER_KEY,
-//     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-//     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-//     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-//   });
-
 const client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
-  });
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
