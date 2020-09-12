8873a8199723b01078562956ffb792f870be85d2
Chris Bell <cbell@acculynx.com>
2020-09-12 18:29:22 -0500

    added typescript

---
+++ dist/index.js
@@ -0,0 +1,18 @@
+var http = require('http');
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+var hostname = '127.0.0.1';
+var port = 3000;
+var server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+server.listen(port, hostname, function () {
+    console.log("Server running at http://" + hostname + ":" + port + "/");
+});
